71c43c4432765afe44a63eea9da3efd00b4dbc98
BNolvia <nolvia.castro@ujcv.edu.hn>
2017-03-20 18:44:08 -0600

    Coneccion base de datos con php

---
+++ mysql_login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOSTNAME", "localhost");// Nombre del host
+define("DATABASE", "u303742148_peli"); // Nombre de la base de datos
+define("USERNAME", "u303742148_peli"); // Nombre del usuario
+define("PASSWORD", "believe"); // Nombre de la constraseña
+?>
